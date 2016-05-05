9fac51d8d5f4ff2d7132dac19974cffacb14cecd
Arnas Kromelis <arnas.kromelis@gmail.com>
2016-05-05 12:28:21 +0200

    List of Available movies

--- VIACinema/Helper.cs
+++ VIACinema/Helper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using VIACinema.Models;
 
 namespace VIACinema
 {
@@ -11,6 +12,42 @@ namespace VIACinema
         {
             var dt = DateTime.Parse(timeString);
             return dt.TimeOfDay;
+        }
+
+
+        public static List<Movie> GetAvailableMovies()
+        {
+            using (CinemaContext db = new CinemaContext())
+            {
+                // Query All Movies
+                var query = from q
+                            in db.Movies
+                            select q;
+
+                // Convert Movie Query to a List
+                var AllMovies = query.ToList();
+
+                // Create an empty Movie list instance to store available movies
+                List<Movie> AvailableMovies = new List<Movie>();
+
+                // For each Movie in All Movies
+                foreach (Movie movie in AllMovies)
+                    if (movie.MovieSessions.Count > 0)
+                        foreach (MovieSession movieSession in movie.MovieSessions)
+                            if (movieSession.SessionDate >= DateTime.Today)
+                            {
+                                // Add movie to the list
+                                AvailableMovies.Add(movie);
+
+                                // Stop checking movie sessions because we just 
+                                // added the movie and don't want any duplicates
+                                // So we break out of the loop...
+                                break;
+                            }
+                                
+
+                return AvailableMovies;
+            }
         }
     }
 }
