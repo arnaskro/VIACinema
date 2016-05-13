cc3edec1f8ebe316bd0d2c3e2801cda9cb96a301
Stabdis <gytis.kuosaitis@gmail.com>
2016-05-13 09:11:18 +0200

    added credit card check

--- VIACinema/AddCreditCard.aspx.cs
+++ VIACinema/AddCreditCard.aspx.cs
@@ -26,21 +26,48 @@ namespace VIACinema
                     newCreditCard.Code = CreditCardNumber.Text;
                     newCreditCard.ExpirationDate = ExpirationDate.Text;
                     newCreditCard.UserId = ((User)Session["user"]).Id;
-
-                    // Check if credit card doesn't exist in the database
-                    if (db.CreditCards.Where(cc => cc.Code == newCreditCard.Code).Count() == 0)
+                    using (var service = new CreditCardValidator.CreditCardValidatorSoapClient("CreditCardValidatorSoap"))
                     {
-                        db.CreditCards.Add(newCreditCard);
-                        db.SaveChanges();
+                        int result = service.ValidCard(newCreditCard.Code, newCreditCard.ExpirationDate);
+                        switch (result)
+                        {
+                            case 0:
+                                if (db.CreditCards.Where(cc => cc.Code == newCreditCard.Code).Count() == 0)
+                                {
+                                    db.CreditCards.Add(newCreditCard);
+                                    db.SaveChanges();
 
-                        (Master as Main).Show_Alert("New Credit Card added!", "success");
-                        Response.Redirect("/UserPage.aspx");
+                                    (Master as Main).Show_Alert("New Credit Card added!", "success");
+                                    Response.Redirect("/UserPage.aspx");
+                                }
+                                else
+                                {
+                                    throw new Exception("Credit Card is already in the database!");
+                                }
+                                break;
+                            case 1001:
+                                (Master as Main).Show_Alert("No credit card number", "error");
+                                break;
+                            case 1002:
+                                (Master as Main).Show_Alert("No expiration date", "error");
+                                break;
+                            case 1003:
+                                (Master as Main).Show_Alert("Invalid card type", "error");
+                                break;
+                            case 1004:
+                                (Master as Main).Show_Alert("Invalid card length", "error");
+                                break;
+                            case 1005:
+                                (Master as Main).Show_Alert("Bad mod 10 check", "error");
+                                break;
+                            case 1006:
+                                (Master as Main).Show_Alert("Bad expiration date", "error");
+                                break;
+                            default:
+                                (Master as Main).Show_Alert("Error", "error");
+                                break;
+                        }
                     }
-                    else
-                    {
-                        throw new Exception("Credit Card is already in the database!");
-                    }
-
                 }
                 catch (Exception ex)
                 {
