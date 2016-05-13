8a42f18095e4b323cc40b166be37afa1b92d22bf
Arnas Kromelis <arnas.kromelis@gmail.com>
2016-05-13 10:01:32 +0200

    adminsitration control visible only to admins

--- VIACinema/Main.Master.designer.cs
+++ VIACinema/Main.Master.designer.cs
@@ -67,6 +67,15 @@ namespace VIACinema {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl UserName;
         
         /// <summary>
+        /// AdministrationControl control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor AdministrationControl;
+        
+        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
