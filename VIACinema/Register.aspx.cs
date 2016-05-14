3d012b7dfea50c2de71a274102571bdf7ae94dd5
Stabdis <gytis.kuosaitis@gmail.com>
2016-05-14 13:07:36 +0200

    Added check if email is in database while registering

--- VIACinema/Register.aspx.cs
+++ VIACinema/Register.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Diagnostics;
 using VIACinema.Models;
 
 namespace VIACinema
@@ -25,20 +26,26 @@ namespace VIACinema
                 try
                 {
                     User u = new Models.User();
-                    CreditCard cc = new Models.CreditCard();
                     u.Name = name.Text;
                     u.Address = address.Text;
                     u.Email = email.Text;
                     u.Password = password.Text;
+                    Debug.WriteLine("Here::: " + db.Users.Where(user => user.Email == u.Email).Count());
+                    if (db.Users.Where(user => user.Email == u.Email).Count() == 0)
+                    {
+                        db.Users.Add(u);
+                        db.SaveChanges();
 
-                    db.Users.Add(u);
-                    db.SaveChanges();
-
-                    Server.Transfer("/Login.aspx", true);
+                        Server.Transfer("/Login.aspx", true);
+                    }
+                    else
+                    {
+                        (Master as Main).Show_Alert("Email is already in the database!", "error");
+                    }
                 }
                 catch (Exception ex)
                 {
-                    (Master as Main).Show_Alert("Incorrect email or password!", "error");
+                    (Master as Main).Show_Alert("Error", "error");
                 }
 
             }
